589e9a8629bedc5d153072fa1743264fe6426940
Rauan Amantaiuly <rauanamantaiuly@MacBook-Air-Rauan.local>
2023-02-17 20:47:43 +0600

    fresh

--- lab3/TO-DO List /action.js
+++ lab3/TO-DO List /action.js
@@ -1,62 +1,43 @@
-window.addEventListener('load', () => {
-	const form = document.querySelector("#new-task-form");
-	const input = document.querySelector("#new-task-input");
-	const list_el = document.querySelector("#tasks");
+const form = document.getElementById("todo-form");
+const input = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
 
-	form.addEventListener('submit', (e) => {
-		e.preventDefault();
+let todos = [];
 
-		const task = input.value;
-
-		const task_el = document.createElement('div');
-		task_el.classList.add('task');
-
-		const task_content_el = document.createElement('div');
-		task_content_el.classList.add('content');
-
-		task_el.appendChild(task_content_el);
-
-		const task_input_el = document.createElement('input');
-		task_input_el.classList.add('text');
-		task_input_el.type = 'text';
-		task_input_el.value = task;
-		task_input_el.setAttribute('readonly', 'readonly');
-
-		task_content_el.appendChild(task_input_el);
-
-		const task_actions_el = document.createElement('div');
-		task_actions_el.classList.add('actions');
-		
-		const task_edit_el = document.createElement('button');
-		task_edit_el.classList.add('edit');
-		task_edit_el.innerText = 'Edit';
-
-		const task_delete_el = document.createElement('button');
-		task_delete_el.classList.add('delete');
-		task_delete_el.innerText = 'Delete';
-
-		task_actions_el.appendChild(task_edit_el);
-		task_actions_el.appendChild(task_delete_el);
-
-		task_el.appendChild(task_actions_el);
-
-		list_el.appendChild(task_el);
-
-		input.value = '';
-
-		task_edit_el.addEventListener('click', (e) => {
-			if (task_edit_el.innerText.toLowerCase() == "edit") {
-				task_edit_el.innerText = "Save";
-				task_input_el.removeAttribute("readonly");
-				task_input_el.focus();
-			} else {
-				task_edit_el.innerText = "Edit";
-				task_input_el.setAttribute("readonly", "readonly");
-			}
-		});
-
-		task_delete_el.addEventListener('click', (e) => {
-			list_el.removeChild(task_el);
-		});
-	});
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const todo = input.value.trim();
+  if (todo.length > 0) {
+    todos.push(todo);
+    renderTodos();
+    input.value = "";
+  }
 });
+
+function renderTodos() {
+  todoList.innerHTML = "";
+  for (let i = 0; i < todos.length; i++) {
+    const li = document.createElement("li");
+    const todo = document.createTextNode(todos[i]);
+    li.appendChild(todo);
+
+    const deleteBtn = document.createElement("button");
+    const deleteText = document.createTextNode("Delete");
+    deleteBtn.appendChild(deleteText);
+    deleteBtn.addEventListener("click", () => {
+      todos.splice(i, 1);
+      renderTodos();
+    });
+    li.appendChild(deleteBtn);
+
+    const completeBtn = document.createElement("button");
+    const completeText = document.createTextNode("Complete");
+    completeBtn.appendChild(completeText);
+    completeBtn.addEventListener("click", () => {
+      li.classList.toggle("complete");
+    });
+    li.appendChild(completeBtn);
+
+    todoList.appendChild(li);
+  }
+}
